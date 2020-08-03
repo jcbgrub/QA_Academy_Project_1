30a061d0c0d25f460562f985c1aee72a923c217a
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-03 20:07:12 +0100

    added layout ext

--- application/routes.py
+++ application/routes.py
@@ -5,3 +5,15 @@ from application import app
 @app.route('/main-lib')
 def home():
  return render_template('main-lib.html', title='My Library')
+
+ @app.route('/login')
+def home():
+ return render_template('login.html', title='login')
+
+ @app.route('/manage-lib')
+def home():
+ return render_template('manage-lib', title='Manage my Entries')
+
+ @app.route('/register)
+def home():
+ return render_template('register.html', title='Register')
