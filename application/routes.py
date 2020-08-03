f7bcc388ae893984bfc68e695c0f3e070685e948
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-03 20:15:52 +0100

    added layout ext

--- application/routes.py
+++ application/routes.py
@@ -2,18 +2,18 @@ from flask import render_template
 from application import app
 
 @app.route('/')
-@app.route('/main-lib')
-def home():
-	return render_template('main-lib.html', title='My Library')
+@app.route('/main_lib')
+def main_lib():
+	return render_template('main_lib.html', title='My Library')
 
 @app.route('/login')
-def home():
+def login():
 	return render_template('login.html', title='login')
 
-@app.route('/manage-lib')
-def home():
-	return render_template('manage-lib', title='Manage my Entries')
+@app.route('/manage_lib')
+def manage_lib():
+	return render_template('manage_lib', title='Manage my Entries')
 
 @app.route('/register')
-def home():
+def register():
 	return render_template('register.html', title='Register')
