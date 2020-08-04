6ae3d73dbf16f321a1896d279e0db79a9ccd09e3
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-04 17:18:06 +0100

    working on forms

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,8 @@ from os import getenv
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
-app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
+# app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
+app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY')
 
 db = SQLAlchemy(app)
 
