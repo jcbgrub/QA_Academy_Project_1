8a7d96df279e91ebd15b373a712f04a8b455bb4f
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-04 16:54:08 +0100

    working on forms

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for
 from application import app, db
-from application.models import Posts
-from application.forms import PostForm
+from application.models import book_lib, users, main_lib
+from application.forms import BookForm
 
 @app.route('/')
 @app.route('/main_lib')
