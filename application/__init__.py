483930e386967f8edf96d23603ba066a2cdc2a25
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-04 19:04:41 +0100

    working on forms

--- application/__init__.py
+++ application/__init__.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
-
 db = SQLAlchemy(app)
 
 from application import routes
--- application/models.py
+++ application/models.py
@@ -10,23 +10,23 @@ class users(db.Model):
     last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
-    main_lib = db.relationship('main_lib', backref='owner', lazy=True)
+    main_lib = db.relationship('main_library', backref='owner', lazy=True)
 
 # Class for book library 
-class book_lib(db.Model):
+class book_library(db.Model):
     book_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     first_name = db.Column(db.String(30), nullable=False)
     surname = db.Column(db.String(30), nullable=False)
     title = db.Column(db.String(100), nullable=False, unique=True)
     pages = db.Column(db.Integer, nullable=False)
     language = db.Column(db.String(30), nullable=False)
-    main_lib = db.relationship('main_lib', backref='bookcode', lazy=True)
+    main_lib = db.relationship('main_library', backref='bookcode', lazy=True)
 
 # Class for the main library 
-class main_lib(db.Model):
+class main_library(db.Model):
     entry_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     user_id = db.Column(db.Integer,db.ForeignKey('users.user_id'))
-    book_id = db.Column(db.Integer,db.ForeignKey('book_lib.book_id'))
+    book_id = db.Column(db.Integer,db.ForeignKey('book_library.book_id'))
     date_read = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     comment = db.Column(db.String(500), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
