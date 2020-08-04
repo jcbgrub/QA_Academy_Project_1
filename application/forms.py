2f7f23ac11a4578c65b62b26fda9642ceba437ac
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-04 23:03:40 +0100

    simplied layout structure

--- application/forms.py
+++ application/forms.py
@@ -5,15 +5,16 @@ from datetime import date
 
 # Insert a new book into the library form
 class BookForm(FlaskForm):
-    first_name = StringField('First Name', validators = [DataRequired(),Length(min=2, max=30)])
-    surname = StringField('Surname',validators = [DataRequired(),Length(min=2, max=30)])
-    title = StringField('Title',validators = [DataRequired(),Length(min=2, max=30)])
-    pages = StringField('Pages',validators = [Length(min=2, max=30)])
-    language = StringField('Language',validators = [DataRequired(),Length(min=2, max=25)])
+    first_name = StringField('First Name', validators = [DataRequired(),Length(min=1, max=30)])
+    surname = StringField('Surname',validators = [DataRequired(),Length(min=1, max=30)])
+    title = StringField('Title',validators = [DataRequired(),Length(min=1, max=30)])
+    pages = StringField('Pages',validators = [Length(min=1, max=30)])
+    language = StringField('Language',validators = [DataRequired(),Length(min=1, max=25)])
     submit = SubmitField('Insert')
 
+# Insert a new rating into the library 
 class RatingForm(FlaskForm):
-    rating =StringField('Rate 1 to 6',validators = [Length(max=1)])
-    comment = StringField('Comments',validators = [Length(min=2, max=1000)])
+    rating =StringField('Rate 1 to 6',validators = [NumberRange(min=1,max=6)])
+    comment = StringField('Comments',validators = [Length(min=1, max=1000)])
     date_read = IntegerField('Date read', validators = [NumberRange(min=1900/1/1,max=date.today())])
     submit = SubmitField('Insert')
--- application/routes.py
+++ application/routes.py
@@ -3,29 +3,22 @@ from application import app, db
 from application.models import book_library, main_library
 from application.forms import BookForm, RatingForm
 
+
+# Route to the login page
 @app.route('/')
-@app.route('/main_lib', methods=['GET', 'POST'])
+@app.route('/login')
+def login():
+	return render_template('login.html', title='login')
+
+# Route to the main library, joining the book and ratings tables.
+@app.route('/main_lib')
 def main_lib():
-	form = RatingForm()
-	if form.validate_on_submit():
-		ratingData = main_library(
-			rating = form.rating.data,
-			comment = form.comment.data,
-			date_read = form.date_read.data
-		)
+	BookData=book_library.query.all().join(main_library)
+	return render_template('main_lib.html', title='My Library')
 
-		db.session.add(RatingForm)
-		db.session.commit()
-
-		return redirect(url_for('main_lib'))
-
-	else:
-		print(form.errors)
-
-	return render_template('main_lib.html', title='My Library', form=form)
-
-@app.route('/manage_lib', methods=['GET', 'POST'])
-def manage_lib():
+# Route to making new book entries
+@app.route('/new_entry', methods=['GET', 'POST'])
+def new_entry():
 	form = BookForm()
 	if form.validate_on_submit():
 		bookData = book_library(
@@ -44,12 +37,35 @@ def manage_lib():
 	else:
 		print(form.errors)
 
-	return render_template('manage_lib.html', title='Manage my Entries', form=form)
+	return render_template('new_entry.html', title='New Entries', form=form)
 
-@app.route('/login')
-def login():
-	return render_template('login.html', title='login')
+# route to rating books
+@app.route('/rate', methods=['GET', 'POST'])
+def rate():
+	form = RatingForm()
+	if form.validate_on_submit():
+		ratingData = main_library(
+			rating = form.rating.data,
+			comment = form.comment.data,
+			date_read = form.date_read.data
+		)
 
+		db.session.add(RatingForm)
+		db.session.commit()
+
+		return redirect(url_for('main_lib'))
+
+	else:
+		print(form.errors)
+
+	return render_template('rate.html', title='Rating', form=form)
+
+# Route to register a new user
 @app.route('/register')
 def register():
 	return render_template('register.html', title='Register')
+
+# Route to update book entries
+@app.route('/update_lib')
+def update_lib():
+	return render_template('update_lib.html', title='Update Entries')
