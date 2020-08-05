d1850fcf43428b25089c491d1aaf44538283c394
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-05 10:34:56 +0100

    setting up login

--- application/models.py
+++ application/models.py
@@ -33,6 +33,8 @@ class book_library(db.Model):
     title = db.Column(db.String(100), nullable=False, unique=True)
     pages = db.Column(db.Integer, nullable=False)
     language = db.Column(db.String(30), nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     main_lib = db.relationship('main_library', backref='bookcode', lazy=True)
 
 # Class for the main library 
