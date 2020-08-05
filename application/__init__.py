2b3cab9cf113297610e02ead282bc433305ea7fc
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-05 09:16:34 +0100

    working on layout

--- application/__init__.py
+++ application/__init__.py
@@ -6,13 +6,13 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 # Imports for running the app locally
-# from os import environ, path
-# from dotenv import load_dotenv
+from os import environ, path
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# basedir = path.abspath(path.dirname(__file__))  # We find the absolute path of the root directory of our current file.
-# load_dotenv(path.join(basedir, '.env'))  # Load our specific .env file from the root directory of our current file.
+basedir = path.abspath(path.dirname(__file__))  # We find the absolute path of the root directory of our current file.
+load_dotenv(path.join(basedir, '.env'))  # Load our specific .env file from the root directory of our current file.
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
--- create.py
+++ create.py
@@ -1,6 +1,5 @@
 from application import db
-from application.models import users, book_library, main_library
 
-db.drop_all()
+# db.drop_all()
 
 db.create_all()
