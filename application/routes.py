d1c702285bebc6eaa30b250e8ebcf07c240f8461
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-06 10:29:48 +0100

    update fucntion

--- application/routes.py
+++ application/routes.py
@@ -104,11 +104,11 @@ def update_lib(book_id):
 		db.session.commit()
 		return redirect(url_for('update_lib'))
 	elif request.method == 'GET':
-		form.first_name = book.first_name.data
-		form.surname = book.surname.data
-		form.title = book.title.data
-		form.pages = book.pages.data
-		form.language = book.language.data
+		form.first_name = book.first_name
+		form.surname = book.surname
+		form.title = book.title
+		form.pages = book.pages
+		form.language = book.language
 	return render_template('update_lib.html', title='Update my library', form=form)
 
 # logout route
