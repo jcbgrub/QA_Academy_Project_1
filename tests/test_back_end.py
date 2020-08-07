f9e5298ba007be248ae400b8f594298607a21488
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-08-07 13:10:28 +0100

    testing in progress

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -134,18 +134,18 @@ class Testadding(TestBase):
 class Testupdating(TestBase):
 	  	# Test that when I update a new book, I am redirected to the homepage with the new post visible
 	def test_editproduct(self):
-        with self.client:
-            self.client.post(
-                url_for("login"),
-                data = dict(
-                    username = "test",
-                    password = "password"
-                ),
-                follow_redirects = True
-            )
-            response = self.client.post(
-                url_for("edit_product", book_id = 1),
-                data = dict(
+		with self.client:
+			self.client.post(
+				url_for("login"),
+				data = dict(
+					username = "test",
+					password = "password"
+				),
+				follow_redirects = True
+			)
+			response = self.client.post(
+				url_for("edit_product", book_id = 1),
+				data = dict(
 					first_name = "Test update name",
 					surname = "Test updatesuname",
 					title = "Test update Title",
@@ -153,6 +153,6 @@ class Testupdating(TestBase):
 					language = "Test update language"
 				),
 				follow_redirects=True
-            )
-            self.assertIn(b"Test update Title", response.data),
-            self.assertEqual(response.status_code, 200)
+			)
+			self.assertIn(b"Test update Title", response.data),
+			self.assertEqual(response.status_code, 200)
